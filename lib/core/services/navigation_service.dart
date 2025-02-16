0d34fc51b0a8fd8b332472945d36712d708080de
p1yux <soulpiyush09@gmail.com>
2025-02-16 13:17:17 +0530

    ...

--- lib/core/services/navigation_service.dart
+++ lib/core/services/navigation_service.dart
@@ -1,1 +1,24 @@
- 
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class NavigationService {
+  static final navigatorKey = GlobalKey<NavigatorState>();
+
+  static NavigatorState get navigator => navigatorKey.currentState!;
+
+  static void navigateToHome(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (context.mounted) {
+        context.go('/home');
+      }
+    });
+  }
+
+  static void navigateToLogin(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (context.mounted) {
+        context.go('/login');
+      }
+    });
+  }
+}
